26654850be129116cbd4f200e5dd6f4a24bd5830
Naveen Panuganti <54768798+Naveen199611@users.noreply.github.com>
2026-05-14 10:18:33 +0530

    Update Product Stock Checker.py

--- Product Stock Checker.py
+++ Product Stock Checker.py
@@ -1,6 +1,6 @@
 #Product Stock Checker
 no_of_products = int(input("enter no of produts available: "))
-if no_of_products>5:
+if no_of_products>=5:
   print("Add to Cart")
 elif no_of_products<5 and no_of_products>0:
   print("Few items left, hurry up! ")
